e7344a9ec6c57e3a4d28702b4a462a890c6c365f
Miguel Vieira <jmvieira@gmail.com>
2016-07-21 11:17:58 +0100

    Added page models for persons.

--- cms/models/behaviours.py
+++ cms/models/behaviours.py
@@ -7,6 +7,16 @@ from wagtail.wagtailcore.fields import StreamField
 from .streamfield import CMSStreamBlock
 
 
+class WithFeedImage(models.Model):
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    class Meta:
+        abstract = True
+
+
 class WithStreamField(models.Model):
     body = StreamField(CMSStreamBlock())
 

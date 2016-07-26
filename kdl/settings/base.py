57490b77983052845e00c1c35ff761ec8c7e2dbd
Ginestra Ferraro <ginestra.ferraro@kcl.ac.uk>
2016-07-26 10:50:03 +0200

    Changed False to None on line 290 for REQUIRE_BUILD_PREFILE in base.py

--- kdl/settings/base.py
+++ kdl/settings/base.py
@@ -287,7 +287,7 @@ REQUIRE_BASE_URL = 'js'
 # Leave blank to use the built-in default build profile. Set to False to
 # disable running the default profile (e.g. if only using it to build
 # Standalone Modules)
-REQUIRE_BUILD_PROFILE = False
+REQUIRE_BUILD_PROFILE = None
 
 # The name of the require.js script used by your project, relative to
 # REQUIRE_BASE_URL.

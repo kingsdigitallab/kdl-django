6e72df8be037ad534fc3526a4307d407caad0505
Miguel Vieira <jmvieira@gmail.com>
2016-10-14 12:39:31 +0100

    App that integrates/interfaces with twitter using twython.

---
+++ twitterhut/settings.py
@@ -0,0 +1,9 @@
+TWITTER_USER_TIMELINE_ITEMS = 3
+
+TWITTER_CACHE_TIMEOUT = 60 * 15
+
+# Don't add the settings below to version control, keep them in the local file
+TWITTER_API_KEY = ''
+TWITTER_API_SECRET = ''
+TWITTER_ACCESS_TOKEN = ''
+TWITTER_ACCESS_TOKEN_SECRET = ''

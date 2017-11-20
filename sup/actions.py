bcf8f8383088f279093471b448a7b096c11cc676
Elliott Hall <elliott.hall@kcl.ac.uk>
2017-11-20 11:57:56 +0000

    Added export to csv for sup

---
+++ sup/actions.py
@@ -0,0 +1,34 @@
+import csv
+import datetime
+
+from django.http import HttpResponse
+
+
+# export selected objects to csv file
+# By defatul exports object using list_fields as the model for a row
+# Add an option export_fields list to modeladmin to change
+def export_to_csv(modeladmin, request, queryset):
+    # Get settigns from model admin
+    if modeladmin.export_fielname:
+        filename = modeladmin.export_fielname
+    else:
+        filename = modeladmin.model.__name__
+    if modeladmin.export_fields:
+        export_fields = modeladmin.export_fields
+    else:
+        export_fields = modeladmin.list_fields
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="{}_{}.csv"'.format(
+        filename,
+        datetime.date.today().replace("-", "_")
+    )
+    writer = csv.writer(response, encoding='utf-8')
+    writer.writerow(export_fields)
+    for obj in queryset:
+        row = [getattr(obj, field)() if callable(getattr(obj, field)) else getattr(obj, field) for field in
+               export_fields]
+        writer.writerow(row)
+    return response
+
+
+export_to_csv.short_description = 'Export selected rows to csv'
--- sup/admin.py
+++ sup/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-
+import actions
 from models import PublicationIdea
 
 
 @admin.register(PublicationIdea)
 class PublicationIdeaAdmin(admin.ModelAdmin):
-    list_display = ['publication_title', 'full_name', 'affiliation']
+    export_filename = 'sup_export'
+    list_display = ['publication_title', 'full_name', 'affiliation',
+                    'country', 'email', 'website', 'publication_title',
+                    'scholarly_discipline', 'keywords', 'summary', 'link'
+                    ]
+    actions = [actions.export_to_csv]
     search_fields = ['publication_title',
                      'full_name', 'affiliation', 'keywords']
